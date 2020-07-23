3b755ecffe6971ef7a115e2f0a6ea9a6d382db85
sprojects18 <info@sgenschow.de>
2020-07-23 17:44:54 +0000

    basic time functionality without days

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from time_calculator import add_time
 from unittest import main
+import replit
+replit.clear() # clear output in console
 
-# /s-projects18/s-projects18-fcc-72
-# fcc-time-calculator
-
-print(add_time("11:06 PM", "2:02"))
-
+#print(add_time("11:06 PM", "2:02"))
+print(1, add_time("3:30 PM", "2:12")) # "5:42 PM"
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,14 @@
-def add_time(start, duration):
+from datetime import timedelta, date, time, datetime
 
+def add_time(start, duration, day=None):
+  # TODO CHECKING
 
+  startTime = datetime.strptime(start, '%I:%M %p')
 
+  sp = duration.split(':')
+  durDelta = timedelta(0, int(sp[0])*60*60 + int(sp[1])*60)
+  nt = startTime + durDelta
+  new_time = nt.strftime('%I:%M %p').strip('0')
 
+  return new_time
 
-    return new_time
