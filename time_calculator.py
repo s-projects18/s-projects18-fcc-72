9520c21425f7e0d445683cef1353b8519df38fa1
sprojects18 <info@sgenschow.de>
2020-07-23 21:29:05 +0000

    input transformations, calculations, introduce weekdays

--- time_calculator.py
+++ time_calculator.py
@@ -1,14 +1,49 @@
-from datetime import timedelta, date, time, datetime
+# Do not import any Python libraries
+# ??? REDO ALL THE STUFF.......
+from datetime import timedelta, datetime
 
 def add_time(start, duration, day=None):
-  # TODO CHECKING
+  days =["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"] 
+  withDay = None
 
-  startTime = datetime.strptime(start, '%I:%M %p')
-
+  # input transformation+checking -------------------
+  if day is not None:
+    day = day.lower()
+    if day not in days:
+      print("Wrong format for: day")
+      return None 
+    for i in range(len(days)):
+      if day==days[i]:
+        weekday='0' + str(i+1) # 01: monday
+        withDay = start + ' '+weekday+'.06.2020' # 01.06.2020 = monday
+        break   
+    
+  try:
+    if withDay is None:
+      startTime = datetime.strptime(start, '%I:%M %p')
+    else:
+      startTime = datetime.strptime(withDay, '%I:%M %p %d.%m.%Y')
+  except:
+    print("Wrong format for: start")
+    return None
+  
   sp = duration.split(':')
+  if len(sp)!=2:
+    print("Wrong format for: duration")
+    return None  
+
+
+  # calclulate ---------------------------------
   durDelta = timedelta(0, int(sp[0])*60*60 + int(sp[1])*60)
   nt = startTime + durDelta
-  new_time = nt.strftime('%I:%M %p').strip('0')
+
+
+  # format output -----------------------------
+  if withDay is None:
+    new_time = nt.strftime('%I:%M %p').strip('0')
+  else:
+    new_time = nt.strftime('%I:%M %p, %A').strip('0')
+    #TODO
 
   return new_time
 
